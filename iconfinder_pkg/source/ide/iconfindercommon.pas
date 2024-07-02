ddfa0b4cd65e827af1824a0d52ae6083eb2fd04e
wp_xyz <wp_xxyyzzz@gmx.net>
2024-07-02 23:37:36 +0200

    Add files missing in previous commit

---
+++ iconfinder_pkg/source/ide/iconfindercommon.pas
@@ -0,0 +1,25 @@
+{
+ **********************************************************************
+  This file is part of a Lazarus Package, IconLib.
+
+  See the file COPYING.modifiedLGPL.txt, included in the Lazarus distribution,
+  for details about the license.
+ **********************************************************************
+}
+
+unit IconFinderCommon;
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+const
+  ICONFINDER_CONFIG_FILENAME = 'iconfindercfg.xml';
+  DEFAULT_IMAGE_FOLDER = 'images/general_purpose/';
+
+implementation
+
+end.
+
